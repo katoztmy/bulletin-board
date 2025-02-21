1af8e554bddd86b741a356f25be9581dacc7f248
katoztmy <kato.eng.dao@gmail.com>
2025-02-21 22:01:01 +0900

    面談後のコメント

--- bulletin-board/src/components/thread/NewCommentForm.jsx
+++ bulletin-board/src/components/thread/NewCommentForm.jsx
@@ -1,6 +1,9 @@
 import { useRef } from "react";
-
+// textareaとかの末端のdom要素はセルフクローズさせられるのであればさせる
+// eslintとかbiomeなど
 export const NewCommentForm = ({ threadId, setComments, fetchComments }) => {
+  // reactHookFormを使うというケースがよくあったりする
+  // reactHookFormによる依存関係を増やしたくないなどの理由で使わないなどもあったりする
   const inputRef = useRef(null);
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,14 +27,12 @@ export const NewCommentForm = ({ threadId, setComments, fetchComments }) => {
     <>
       <div className="post-form">
         <form onSubmit={handleSubmit}>
-          <textarea
-            name="投稿"
-            placeholder="投稿しよう！"
-            ref={inputRef}
-          ></textarea>
+          <textarea name="投稿" placeholder="投稿しよう！" ref={inputRef} />
           <button type="submit">投稿</button>
         </form>
       </div>
     </>
   );
 };
+// useCommentForm()のようなコンポーネントに対応するカスタムフックを使用して、handleSubmitやinputRefなどのロジック部分を閉じ込める
+// UIとロジックを分離させる、自動テストを行うためにも切り出せる
